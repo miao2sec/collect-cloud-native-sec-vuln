a2e997d139b8478c46ce331b869f5a051a994db3
Yaney <yangli.yaney@foxmail.com>
2024-12-20 17:07:41 +0800

    Add Github

---
+++ scripts/update.sh
@@ -0,0 +1,28 @@
+#!/bin/bash -eu
+
+
+COMMIT_MSG=$2
+
+
+
+if [ -z "$COMMIT_MSG" ]; then
+  echo "commit message required"
+  exit 1
+fi
+
+result=0
+./collect -r -c "$VULN_LIST_DIR"  || result=$?
+
+if [ $result -ne 0 ]; then
+  echo "[Err] Revert changes" >&2
+  cd "$VULN_LIST_DIR" && git reset --hard HEAD
+  exit 1
+fi
+
+cd "$VULN_LIST_DIR" || exit 1
+
+if [[ -n $(git status --porcelain) ]]; then
+  git add .
+  git commit -m "${COMMIT_MSG}"
+  git push
+fi
